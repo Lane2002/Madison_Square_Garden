6056cd10fea34c60e0e7b044ca2cf8d9ca9b7518
Lane2002 <woodelaine2002@hotmail.co.uk>
2021-07-01 10:17:31 +0100

    first commit

---
+++ files/main.js
@@ -0,0 +1,12 @@
+$(document).ready(() => {
+    $('#cart').on('click', () => {
+      $("#cartMenu").show()
+    });
+    $('#account').on('click', () => {
+      $("#accountMenu").show()
+    });
+  
+  $('.dropdown-menu').on('mouseleave', () => {
+    $('dropdown-menu').hide()
+  });
+  })
